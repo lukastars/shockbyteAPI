844265d6ec6ddd904c3d263b0bcf43d24ef4695f
luka_stars <bonger2014@gmail.com>
2021-10-14 05:01:04 +0200

    fix  loading spinner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,6 +53,8 @@ export default {
   },
   methods: {
     async getNodesData() {
+      store.commit('changeLoading', true);
+
       try {
         const response = await axios.get(
           `https://600f10ec6c21e1001704e67a.mockapi.io/api/v1/stats`
--- src/views/NodeInfo.vue
+++ src/views/NodeInfo.vue
@@ -79,6 +79,8 @@ export default {
   },
   methods: {
     async getNodeData(id) {
+      store.commit('changeLoading', true);
+
       try {
         const response = await axios.get(
           `https://600f10ec6c21e1001704e67a.mockapi.io/api/v1/stats/${id}`
